bebee8dbb3fd3a9c879ea6d3d10677e298aa5e31
jatin foujdar <jatin321001@gmail.com>
2022-12-07 23:17:56 +0530

    userSchema commit

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
 
 const userSchema = mongoose.Schema(
     {
@@ -7,6 +8,30 @@ const userSchema = mongoose.Schema(
             required:[true,"Name is required"],
             maxLength:[20,"Name must be less than 20"],
             trim:true
-        }
+        },
+        email:{
+            type:String,
+            required:[true,"Email is required"],
+            unique:true,
+            trim:true
+        },
+        password:{
+            type:String,
+            required:[true,"password is required"],
+            minLength:[10,"password must be at least 10 char"],
+            select:false
+        },
+        role:{
+            type:String,
+            enum: Object.values(AuthRoles),
+            default: AuthRoles.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry : Date,
+    },
+    {
+        timestamps:true,
     }
-)
+);
+
+export default mongoose.model("User",userSchema);
