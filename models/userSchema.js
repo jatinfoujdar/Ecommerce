db830cb209668d2b0d869c3a5efd42aafcd32339
jatin foujdar <jatin321001@gmail.com>
2022-12-08 02:06:54 +0530

    generate token

--- models/userSchema.js
+++ models/userSchema.js
@@ -3,7 +3,7 @@ import AuthRoles from "../utils/authRoles";
 import bcrypt from "bcryptjs";
 import JWT from "jsonwebtoken";
 import crypto from "crypto";
-
+import config from "../config/index";
 
 
 const userSchema = mongoose.Schema(
@@ -45,5 +45,26 @@ userSchema.pre("save", async function(next){
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
+//add more feature directly to schema
+userSchema.method={
+    //compare password
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+    //generate jwt token
+       getJwtToken: function(){
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+            }
+        )
+    }
+}
+
 
 export default mongoose.model("User",userSchema);
