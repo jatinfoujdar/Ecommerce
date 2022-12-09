205b202c187350a7a56b660366024e75ab0759f1
jatin foujdar <jatin321001@gmail.com>
2022-12-09 18:13:19 +0530

    product schema commit

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,10 +1,48 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.productSchema({
-
+const productSchema = new mongoose.productSchema(
+    {
     name:{
         type:String,
-        
+        required:[true,"provide a product name"],
+        trim:true,
+        maxLength:[120,"product name should be in 120 char "]
+    },
+    price:{
+        type:Number,
+        required:[true,"provide a product price"],
+        maxLength:[6,"product price should not be more than 6 num "]
+    },
+    description:{
+        type:String,
+    //use editor - PA       
+    },
+    photos:[
+        {
+        url:{
+            type:String,
+            required:true
+           }
     }
+  ],
+  stock:{
+    type:Number,
+    default:0
+  },
+  sold:{
+    type:Number,
+    default:0
+  },
+ 
+  collectionId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Collection"
+  }
+},
+   {
+    timestamps:true
+   }
+)
 
-})
+
+export default mongoose.model("Product",productSchema);
