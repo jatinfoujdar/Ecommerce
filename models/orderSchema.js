05b83bff8264986220f4b48293138c7628feb8d3
jatin foujdar <jatin321001@gmail.com>
2022-12-10 01:32:34 +0530

    oder schema commit

--- models/orderSchema.js
+++ models/orderSchema.js
@@ -3,9 +3,49 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
       
     {
-       
-    }
-  
+       products:{
+
+        type:[{
+            productId:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Product",
+                required:true,
+            },
+            count:Number,
+            price:Number
+        }],
+        required:true
+             
+       },
+       user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "USer",
+        required:true
+    },
+    phoneNumber:{
+        type:Number,
+        required:true,
+    },
+    amount:{
+        type:Number,
+        required:true,
+    },
+    coupon:String,
+    transactionId: String,
+    address:{
+        type:String,
+        required:true,
+    },
+    status:{
+        type:String,
+        enum:[""]
+        //improve !!!
+    },
+    //pay mode
+    },
+  {
+    timeStamps:true
+  }
 )
 
 
